09ca5baff7fae6ed5983014734a57aff7b7a6e7b
Danny Lam <dannylam81@gmail.com>
2017-02-23 17:41:17 +0100

    added buttons in play state

--- common/js/menu.js
+++ common/js/menu.js
@@ -1,6 +1,7 @@
 var bg;
 var title_txt;
 var start_btn;
+var start_btn_txt;
 var gameX;
 var gameY;
 var gameW;
@@ -8,6 +9,14 @@ var gameH;
 
 var style = {
     font: "60px Arial",
+    fill: "#ffffff",
+    wordWrap: true,
+    wordWrapWidth: 1000,
+    align: "center",
+    fontWeight: "bold"
+}
+var btn_style = {
+    font: "40px Arial",
     fill: "#ffffff",
     wordWrap: true,
     wordWrapWidth: 1000,
@@ -27,14 +36,39 @@ var menu = {
         bg = game.add.image(0, 0, 'background');
         title_txt = game.add.text(gameX, (gameY/4), 'Follow my Lead', style);
         title_txt.anchor.set(0.5);
-        start_btn = game.add.button(gameX, gameY, 'button', startGame);
+        
+        start_btn = game.add.sprite(gameX, gameY, 'button');
+        start_btn.scale.set(0.5);
+        start_btn.inputEnabled = true;
         start_btn.anchor.set(0.5);
+       
+        start_btn_txt = game.add.text((gameX+5), (gameY+3), 'START', btn_style);
+        start_btn_txt.anchor.set(0.5);
+        start_btn.my_txt = start_btn_txt; //link this txt to this btn
+        
+        start_btn.input.useHandCursor = true;
+        
+        start_btn.events.onInputDown.add(onInputDown, this);
+        start_btn.events.onInputUp.add(onInputUp, this);
     }
     
 }
 
 function startGame() {
     
-    game.state.start('Play');
+    //game.state.start('Play');
     
+}
+
+function onInputDown(target) {    
+    game.add.tween(target.scale).to({x: 0.4, y: 0.4}, 100, Phaser.Easing.Cubic.Out, true);
+    game.add.tween(target.my_txt.scale).to({x: 0.9, y: 0.9}, 100, Phaser.Easing.Cubic.Out, true);
+}
+function onInputUp(target) {
+    //console.log('button up', arguments);
+    game.add.tween(target.scale).to({x: 0.5, y: 0.5}, 100, Phaser.Easing.Cubic.Out, true);
+    game.add.tween(target.my_txt.scale).to({x: 1, y: 1}, 100, Phaser.Easing.Cubic.Out, true);
+    if(arguments[2]) {
+        game.state.start('Play');
+    }
 }
--- common/js/play.js
+++ common/js/play.js
@@ -1,5 +1,13 @@
 var play_txt;
 var finish_btn;
+var btn1, btn2, btn3, btn4;
+var xMargin, yMargin;
+var btnArray;
+var comboArray;
+var totalBtn;
+
+var timer;
+var level = 0;
 
 var play = {
     
@@ -7,8 +15,59 @@ var play = {
         
         bg = game.add.image(0, 0, 'background');
         play_txt = game.add.text(gameX, (gameY/4), 'End this game', style);
+        play_txt.anchor.set(0.5);
         
-        finish_btn = game.add.button(gameX, gameY, 'button', finishGame);
+        timer = game.time.create(false);
+        timer.loop(500, followMyLead, this);
+        timer.start();
+        
+        xMargin = 40;
+        yMargin = 40;
+        
+        btn1 = game.add.sprite(gameW/4-xMargin, (gameH/4*3+yMargin), 'square');
+        btn1.anchor.set(0.5);
+        btn1.scale.set(0.6);
+        btn1.alpha = 0.5;
+        btn1.value = 1;
+        btn1.inputEnabled = true;
+        btn1.input.useHandCursor = true;
+        btn1.events.onInputDown.add(pressThis, this);
+        btn1.events.onInputUp.add(releaseThis, this);
+        
+        btn2 = game.add.sprite(gameW/2, (gameH/4*3-140+yMargin), 'triangle');
+        btn2.anchor.set(0.5);
+        btn2.scale.set(0.6);
+        btn2.alpha = 0.5;
+        btn2.value = 2;
+        btn2.inputEnabled = true;
+        btn2.input.useHandCursor = true;
+        btn2.events.onInputDown.add(pressThis, this);
+        btn2.events.onInputUp.add(releaseThis, this);
+        
+        btn3 = game.add.sprite(gameW/2, (gameH/4*3+140+yMargin), 'cross');
+        btn3.anchor.set(0.5);
+        btn3.scale.set(0.6);
+        btn3.alpha = 0.5;
+        btn3.value = 3;
+        btn3.inputEnabled = true;
+        btn3.input.useHandCursor = true;
+        btn3.events.onInputDown.add(pressThis, this);
+        btn3.events.onInputUp.add(releaseThis, this);
+        
+        btn4 = game.add.sprite((gameW/4*3+xMargin), (gameH/4*3+yMargin), 'circle');
+        btn4.anchor.set(0.5);
+        btn4.scale.set(0.6);
+        btn4.alpha = 0.5;
+        btn4.value = 4;
+        btn4.inputEnabled = true;
+        btn4.input.useHandCursor = true;
+        btn4.events.onInputDown.add(pressThis, this);
+        btn4.events.onInputUp.add(releaseThis, this);
+        
+    },
+    
+    update: function() {
+        
     }
     
 }
@@ -16,5 +75,25 @@ var play = {
 function finishGame() {
     
     game.state.start('GameOver');
+    
+}
+
+function pressThis(target) {    
+    game.add.tween(target.scale).to({x: 0.5, y: 0.5}, 100, Phaser.Easing.Cubic.Out, true);
+    target.alpha = 1;
+}
+function releaseThis(target) {
+    //console.log('button up', arguments);
+    game.add.tween(target.scale).to({x: 0.6, y: 0.6}, 100, Phaser.Easing.Cubic.Out, true);
+    target.alpha = 0.5;
+    if(arguments[2]) {
+        console.log(target.value);
+    }
+}
+
+
+function followMyLead(){
+    
+    
     
 }
