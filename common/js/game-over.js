338d3dab5c673ec8bc8e49a37cbfdec730133739
Danny Lam <dannylam81@gmail.com>
2017-02-24 17:15:47 +0100

    * copy game-over-screen to master working tree

--- common/js/game-over.js
+++ common/js/game-over.js
@@ -7,8 +7,10 @@ var gameOver = {
         
         bg = game.add.image(0, 0, 'background');
         restart_txt = game.add.text(gameX, (gameY/4), 'Game Over', style);
-        
+        restart_txt.anchor.set(0.5);
         restart_btn = game.add.button(gameX, gameY, 'button', restartGame);
+        restart_btn.anchor.set(0.5);
+        restart_btn.events.onInputDown.add(onInputDown, this);
     }
     
 }
